acd4f2a6f2c679cd1bb31166ae359ce84d0a9117
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2023-12-18 13:12:16 +0530

    correctly working pre-requisites

--- install.sh
+++ install.sh
@@ -4,10 +4,80 @@
 ################################################################################
 #!/bin/bash
 # declaration of variables for location storage
+echo "################################################################################"
+echo "# Organization : Tarcin Robotic LLP"
+echo "# Author       : vigneshpandian"
+echo "################################################################################"
+echo " "
 
-sudo apt-get install postgresql-12;
-sudo -u postgres createuser canvas --no-createdb --no-superuser --no-createrole --pwprompt; 
-sudo -u postgres createdb canvas_production --owner=canvas; 
-sudo -u postgres createdb canvas_development --owner=canvas; 
-sudo -u postgres createuser $USER;
-sudo -u postgres psql -c "alter user $USER with superuser" postgres;
+# updating server 
+echo    "---- UPDATING SERVER ----"
+echo    " "
+if sudo apt update && sudo apt upgrade -y ; then
+    echo "---- updation completed !!! ----"
+else
+    echo "---- server updation failed ----"
+fi
+
+# postgres area
+echo " "
+echo "★ Create a PostgreSQL user and databases for Canvas"
+echo " "
+#installing postgresql
+echo "---- installing postgresql ---- "
+echo " "
+if sudo apt-get install postgresql; then
+    echo " ✓ postgresql installed successfully ✓"
+else
+    echo " ✘ postgres installation failed ✘ "
+fi
+
+# creating canvas user
+echo " "
+echo "★ Creating canvas user in postgresql"
+echo " "
+if sudo -u postgres createuser canvas --no-createdb --no-superuser --no-createrole --pwprompt; then
+    echo "✓ successfully created canvas user"
+else
+    echo "✘ failed in creating canvas user"
+fi
+
+# creating canvas_production db
+echo " "
+echo "★ Creating canvas_production database"
+echo " "
+if sudo -u postgres createdb canvas_production --owner=canvas ; then 
+    echo "✓ successfully created canvas_production database"
+else
+    echo "✘ failed in creating canvas_production database"
+fi
+
+# creating canvas_dev database 
+echo " "
+echo "★ Creating canvas_development database"
+echo " "
+if sudo -u postgres createdb canvas_development --owner=canvas; then
+    echo "✓ successfully created canvas_development database"
+else
+    echo "✘ failed in creating canvas_development database"
+fi
+
+# creating postgres user for $USER
+echo " "
+echo "★ Creating postgres for user $USER "
+echo " "
+if sudo -u postgres createuser $USER; then 
+    echo "✓ successfully created postgres user"
+else    
+    echo "✘ failed in creating postgres user"
+fi
+
+# altering user in postgresql with superuser
+echo " "
+echo "★ Altering user in postgresql with superuser"
+echo " "
+if sudo -u postgres psql -c "alter user $USER with superuser" postgres; then
+    echo "✓ successfully altered $USER with superuser"
+else
+    echo "✘ failed in altering $USER with superuser"
+fi
