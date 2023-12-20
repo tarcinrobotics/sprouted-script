cfb7e019c5f7e9ba9f1d1cd90657cf6e757a6b8f
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2023-12-20 11:21:41 +0530

    install3 file is for writing configurations in config dir

---
+++ install3.sh
@@ -0,0 +1,83 @@
+# declarations for file configuration
+
+
+# configuring database, outgoing mail, domain settings
+
+# copying database file 
+database_file="/var/canvas/config/database.yml"
+dynamic_settings_file="/var/canvas/config/dynamic_settings.yml"
+outgoing_mail_file="/var/canvas/config/outgoing_mail.yml"
+security_mail_file="/var/canvas/config/security.yml"
+outgoing_mail_content='production:
+  delivery_method: "smtp"
+  address: "smtp.gmail.com"
+  enable_starttls_auto: true
+  port: "587"
+  user_name: "tarcinrobotics301@gmail.com"
+  password: "ryzjskxdibavwhbd"
+  authentication: "plain" # plain, login, or cram_md5
+  domain: "localhost"
+  outgoing_address: "tarcinrobotics301@gmail.com"
+  default_name: "SproutED - Unlock the Tech, Unlock the World"'
+
+security_mail_content="
+production: &default
+
+# replace this with a random string of at least 20 characters
+
+encryption_key: daedd3a131ddd8988b14f6e4e01039c93cfa0160
+
+lti_iss: '{your_domain}'"
+
+if cp /var/canvas/config/database.yml.example /var/canvas/config/database.yml ; then
+    echo "✓ successfully copied the database file"
+else
+    echo "✘ failed in copying the database file"
+fi
+
+# copying dynamic_settings file
+if cp /var/canvas/config/dynamic_settings.yml.example /var/canvas/config/dynamic_settings.yml ; then
+    echo "✓ successfully copied the dynamic_settings file"
+else
+    echo "✘ failed in copying the dynamic_settings file"
+fi
+
+# copying outgoing_mail file 
+if cp /var/canvas/config/outgoing_mail.yml.example /var/canvas/config/outgoing_mail.yml ; then
+    echo "✓ successfully copied the outgoing_mail file"
+else
+    echo "✘ failed in copying the outgoing_mail file"
+fi
+
+# editing config files
+
+# databse.yml file
+
+if sed -i 's/password: your_password/password: canvas/g' "$database_file" ; then    
+    echo "✓ changed password to canvas to database.yml file"
+else    
+    echo "✘ failed in changing the password to database.yml file / the password has been already changed"
+fi
+
+# dynamic_settings.yml file
+if sed -i 's/development/production/g' "$dynamic_settings_file" ; then  
+    echo "✓ changed the dynamic_settings file"
+else
+    echo "✘ failed to change the dynamic_settings file / the file has been already changed"
+fi
+
+# outgoing_mail file
+awk -v new_config="$outgoing_mail_content" '/production:/ {print new_config; found=1; next} found && /^\s*$/{found=0} !found' "$outgoing_mail_file" > "$outgoing_mail_file.tmp"
+if mv "$outgoing_mail_file.tmp" "$outgoing_mail_file" ; then 
+    echo "✓ outgoing_mail configured successfully"
+else
+    echo "✘ outgoing_mail failed to configure"
+fi
+
+# security file
+awk -v sec_config="$security_mail_content" '/production:/ {print sec_config; found=1; next} found && /^\s*$/{found=0} !found' "$security_file" > "$security_file.tmp"
+if mv "$security_file.tmp" "security_file" ; then
+    echo "✓ security file configured successfully"
+else
+    echo "✘ security file failed to configure"
+fi
